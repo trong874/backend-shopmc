8eef82aa87f33773a46ccc7e0177c0edefad8536
trong874 <pvt874@gmail.com>
2021-11-08 18:19:51 +0700

    as

--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -90,7 +90,7 @@
                                         data-dismiss="modal">Close
                                 </button>
                                 <p class="btn btn-danger font-weight-bold" id="delete_all"
-                                   data-url="/admin/delete-many-user">Xóa</p>
+                                   data-url="{{ route('user_qtv.destroy_many') }}">Xóa</p>
                             </div>
                         </div>
                     </div>
