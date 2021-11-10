356c95296b0d889f90d2df1a8e1f079faa50624d
trong874 <pvt874@gmail.com>
2021-11-10 17:32:39 +0700

    upload multi image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-            //
+        if ($_SERVER['SERVER_NAME'] !== '127.0.0.1') {
+            URL::forceScheme('https');
+        }
     }
 }
