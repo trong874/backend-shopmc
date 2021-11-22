6e4b619baa67017ce92a14889f39d50544628003
tutepp <tutepp>
2021-11-22 15:32:33 +0700

    fix cart

--- widget/widget.php
+++ widget/widget.php
@@ -160,10 +160,12 @@ View::composer('frontend.layout.core.hearder', function ($view) {
     if (empty($user)){
         {
             $data_cart = 0;
+            dd($data_cart);
         }
     }
     else{
         $cart = Cart::with('items')->where('user_id',$user->id)->first();
+        dd($cart->id);
         $cart_item_of_cart = Cart_Item::where('cart_id',$cart->id)->get();
         $data_cart = [
             'items'=>$cart->items,
